6435e89601b53578ed24b36257a7fa23e89dc5e8
PraudMaelle <75522401+PraudMaelle@users.noreply.github.com>
2020-12-05 16:30:40 +0100

    Update modalgo.f90

--- modalgo.f90
+++ modalgo.f90
@@ -8,39 +8,38 @@ module modalgo
 
 contains
 
-  !produit matrice matrice
-  !produit matrice vecteur
-  !norme p
-  !norme infinie
-  !transposÃ©e d'une matrice
-  !trace
+!=================================================================================
 
-
-    !fonction pour la mÃ©thode de gradient Ã  pas optimal
+    !fonction pour la methode de gradient a  pas optimal
 
     function grad_pas_optimal(A,b,x0,kmax,e) result(x)
 
-      !dÃ©claration des arguments
-      !prÃ©cision
-      real(kind=Pr), intent(in):: e
-      !test d'arrÃªt
-      integer, intent(in)::kmax
-      real(kind=Pr), dimension(:,:), intent(in)::A
-      real(kind=Pr),dimension(:), intent(in):: b, x0
-      real(kind=Pr), dimension(:),allocatable::x
-      real(kind=Pr), dimension(:), allocatable :: r,z
+      !declaration des arguments
+      real(kind=Pr), intent(in):: e                     !precision
+      integer, intent(in)::kmax                         !test d'arret
+      real(kind=Pr), dimension(:,:), intent(in)::A      !matrice du systeme lineaire
+      real(kind=Pr),dimension(:), intent(in):: b, x0    !second membre + vecteur initial
+      real(kind=Pr), dimension(:),allocatable::x        !solution approchee
+
+      !varaibles locales
+      real(kind=Pr), dimension(:), allocatable :: r   !residu reel
+      real(kind=Pr), dimension(:), allocatable :: z
       real(kind=Pr) :: alpha, beta
       integer :: k, n
 
+      !initialisation
       n=size(A)
       allocate(z(n))
       allocate(r(n))
       allocate(x(n))
 
+      x=x0
       r=b-matmul(A,x0)
       beta=NORM2(r)
       k=0
 
+
+      !algorithme de la methode 
       do while (k<=kmax .and.beta>e )
          z=matmul(A,r)
          alpha=DOT_PRODUCT(r,r)/DOT_PRODUCT(z,r)
@@ -52,47 +51,61 @@ contains
       end do
 
       if (k>kmax) then
-         print*, "tolÃ©rance atteinte"
+         print*, "tolerance non atteinte", NORM2(r)
       end if
-      end function
+
+      deallocate(r,z,x)
+      
+    end function grad_pas_optimal
       
       
 !====================================================================================    
-    !fonction pour la mÃ©thode du rÃ©sidu minimum
+    !fonction pour la methode du residu minimum
 
-    function res_min(A,b,x0,kmax,e )result(x)
+    function res_min(A,b,x0,kmax,e)result(x)
 
-      !DÃ©claration des variables
-      real(kind=Pr), intent(in):: e
-      !ItÃ©ration maximale
-      integer, intent(in)::kmax
-      real(kind=Pr), dimension(:,:), intent(in)::A
-      real(kind=Pr),dimension(:), intent(in):: b, x0
-      real(kind=Pr), dimension(:),allocatable::x
+      !Declaration des variables
+      real(kind=Pr), intent(in):: e                      !precision
+      integer, intent(in)::kmax                          !test d'arret
+      real(kind=Pr), dimension(:,:), intent(in)::A       !matrice du syteme lineaire
+      real(kind=Pr),dimension(:), intent(in):: b, x0     !second membre + vecteur initial
+
+      !sortie
+      real(kind=Pr), dimension(:),allocatable::x         !solution approchee 
+
+      !variables locales
       real(kind=Pr), dimension(:), allocatable :: r,z
-      real(kind=Pr) :: alpha
+      real(kind=Pr) :: alpha, beta
       integer :: k, n
 
-      n=size(A)
-      allocate(z(n))
-      allocate(r(n))
-      allocate(x(n))
 
+      !initialisation
+      n=size(A)
+      allocate(z(n),r(n),x(n))
+      x=x0
       r=b-matmul(A,x0)
+      beta=NORM2(r)
+      z=0._pr
       k=0
 
-      do while (k<=kmax .and. NORM2(r) > e)    !Fonction norme Ã  dÃ©finir
+      !algorithme de la methode
+      do while (k<=kmax .and. beta> e)    !Fonction norme Ã  dÃ©finir
          z=matmul(A,r)
          alpha=DOT_PRODUCT(r,z)/DOT_PRODUCT(z,z)
          x=x+alpha*r
          r=r-alpha*z
+         beta=NORM2(r)
          k=k+1
 
       end do
 
+      
+
       if (k>kmax) then
-         print*, "tolÃ©rance atteinte", NORM2(r)
+         print*, "tolerance non atteinte", NORM2(r)
       end if
+
+      deallocate(z,r,x)
   end function res_min
 
 
@@ -100,7 +113,10 @@ contains
 
 
 
-  !===================================================================================
+  !=======================================================================================================
+
+!fonction pour la methode du gradient conjugue
+  
 function gradient_conjugue(A,b,x0,kmax,e) result(x)
     
     !variables d'entree
@@ -123,9 +139,10 @@ function gradient_conjugue(A,b,x0,kmax,e) result(x)
     r=b-MATMUL(A,x0)
     beta=NORM2(r)
     p=r
+    
     k=0
 
-    !algorithme 
+    !algorithme de la methode 
     do while (beta>e.and.k <= kmax)
        z=MATMUL(A,p)
        w=DOT_PRODUCT(r,r)
@@ -139,24 +156,32 @@ function gradient_conjugue(A,b,x0,kmax,e) result(x)
        k=k+1
     end do
 
+    
+
     if(k>kmax) then
-       print*, "tolérance non atteinte:" ,  beta
+       print*, "tolerance non atteinte:" ,  beta
     end if
     
-    deallocate(r,rplus,p,z)
+    deallocate(r,rplus,p,z,x)
 
     
   end function gradient_conjugue
 
+
+  !===================================================================================================================
+
+!algorithme d'Arnoldi: construction de Vm et Hm
   
   subroutine Arnoldi(v,A,m,Hm,Vm)
 
-    !variables d'entrees 
-    real(kind=pr),dimension(:),intent(in)::v
+    !variables d'entrees
     real(kind=pr),dimension(:,:),intent(in):: A
+    real(kind=pr),dimension(:),intent(in)::v                !vecteur qui definit l'espace de Krylov {v,Av,.....,A(m-1)v}
+   
     !variables de sortie
-    real(kind=pr),dimension(:,:),allocatable,intent(out):: Vm
-    real(kind=pr),dimension(:,:),allocatable,intent(out):: Hm
+    real(kind=pr),dimension(:,:),allocatable,intent(out):: Vm     !matrice qui contient les vecteurs de la nouvelle base orthonormale
+    real(kind=pr),dimension(:,:),allocatable,intent(out):: Hm     !matrice de Hessenberg qui contient les coefficients de la methode de Gram-Schmidt
+    
     !variables locales
     integer:: m, n, i, j
     real(kind=pr),dimension(:,:),allocatable:: vecteurv
@@ -191,32 +216,138 @@ function gradient_conjugue(A,b,x0,kmax,e) result(x)
        
     end do
 
+    deallocate(Vm,Hm,vecteurv,wj)
     
   end subroutine Arnoldi
 
 
+  !============================================================================================================
+
+!fonction qui contient la methode de resolution FOM
+
+  
   function FOM(A,b,x0,kmax,e) result(x)
      !variables d'entree
-    real(kind=pr),dimension(:,:),intent(in):: A       !matrice de résolution du probleme
+    real(kind=pr),dimension(:,:),intent(in):: A       !matrice du systeme lineaire
     real(kind=pr),dimension(:),intent(in):: b, x0     !vecteur second membre et donnée intiale
     integer,intent(in):: kmax                         !test d'arret
     real(kind=pr),intent(in):: e                      !precision
     !variables de sortie
     real(kind=pr),dimension(:),allocatable:: x        !solution approchée du systeme
     !variables locales
-    real(kind=pr),dimension(:),allocatable:: r
+    real(kind=pr),dimension(:),allocatable:: r        !residu reel
+    real(kind=pr),dimension(:),allocatable:: y
     real(kind=pr):: beta
-    integer:: k
+    integer:: k, m, n
+    real(kind=pr),dimension(:,:),allocatable:: Hm, Vm
+    
 
     !initialisation
-    r=b-MATMUL(A,x0)
+    n=size(A)
+    allocate(r(n),x(n))
+    x=x0
+    y=0._pr
+    r=b-MATMUL(A,x0)     !residu initial
     beta=NORM2(r)
+    
+    m=2
+    allocate(y(m))
+    y=0._pr
+    allocate(Hm(m+1,m))
+    allocate(Vm(n,m+1))
+    Hm=0._pr
+    Vm=0._pr
+    
     k=0
 
-    do while (beta>e.and.k <= kmax)
+
+    !algorithme de la methode
+    
+    do while (beta>e.and.k<= kmax)
+       !obtention de Hm et Vm en partant de r
+       call Arnoldi(r,A,m,Hm,Vm)
+       !résolution de Hmy=betae1
+       y(1)=beta/Hm(1,1)
+       x=x+MATMUL(Vm(1:n,1:m),y)
+       r=-Hm(m+1,m)*y(m)*Vm(1:n,m+1)
+       beta=NORM2(r)
+       k=k+1    
     end do
+
+
+    
+    if (k>kmax) then
+       print*, 'tolerance non atteinte' , beta
+    end if
+
+    deallocate(r,y,Hm,Vm,x)
+    
   end function FOM
 
+!================================================================================================================
+
+!fonction pour la methode GMRes
+
+  
+  function GMRes(A,b,x0,kmax,e)  result(x)
+
+    !variables d'entree
+    real(kind=pr),dimension(:,:),intent(in):: A       !matrice de résolution du probleme
+    real(kind=pr),dimension(:),intent(in):: b, x0     !vecteur second membre et donnée intiale
+    integer,intent(in):: kmax                         !test d'arret
+    real(kind=pr),intent(in):: e                      !precision
+
+    !variables de sortie
+    real(kind=pr),dimension(:),allocatable:: x        !solution approchée du systeme
+
+    !variables locales
+    real(kind=pr),dimension(:),allocatable:: r, y, betae1
+    real(kind=pr):: beta
+    integer:: n,m,k
+    real(kind=pr),dimension(:,:),allocatable:: Hm, Vm
+    
+    !initialisation
+    n=size(A)
+    
+    allocate(x(n),r(n),betae1(n))
+    x=x0
+    r=b-MATMUL(A,x0)
+    beta=NORM2(r)
+
+    betae1=0._pr
+    
+    m=2
+    allocate(y(m))
+    y=0._pr
+    
+    allocate(Hm(m+1,m))
+    allocate(Vm(n,m+1))
+    Hm=0._pr
+    Vm=0._pr
+    
+    k=0
+
+    !algorithme de la  methode
+
+    do while (beta>e.and.k<= kmax)
+       call Arnoldi(r,A,m,Hm,Vm)
+       !calcul de y=argmin(beta e1 - Hmy)
+
+       x=x+MATMUL(Vm(1:n,1:m),y)
+       !definition de betae1
+       betae1(1)=beta
+       r=betae1-MATMUL(Hm(1:m,1:m),y)
+       beta=NORM2(r)
+       k=k+1
+       
+    end do
+
+    
+    if (k>kmax) then
+       print*, 'tolerance non atteinte', beta
+    end if
+    
+  end function GMRes
   
  !====================================================================================
      !Fonction pour le calcul de la normeInf
