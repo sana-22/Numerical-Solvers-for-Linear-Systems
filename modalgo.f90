8fc0eb88ab0eb387d9007b9e25fe27c5a5a1a45d
PraudMaelle <75522401+PraudMaelle@users.noreply.github.com>
2020-12-12 18:52:57 +0100

    Update modalgo.f90

--- modalgo.f90
+++ modalgo.f90
@@ -215,10 +215,7 @@ function gradient_conjugue(A,b,x0,kmax,e) result(x)
     allocate(Vmplus(n,m+1))
 
     
-    allocate(Qm(m,m),Rm(m,m))
-    Qm=0._pr
-    Rm=0._pr
-    
+    allocate(Qm(m,m),Rm(m,m)) 
     allocate(u(m),betae1(m))
     u=0._pr
     betae1=0._pr
@@ -232,6 +229,9 @@ function gradient_conjugue(A,b,x0,kmax,e) result(x)
 
        Hm=0._pr
        Vmplus=0._pr
+       Qm=0._pr
+       Rm=0._pr
+       
        !obtention de Hm et Vm par la methode d'Arnoldi en partant de r et A
        call Arnoldi(r,A,Hm,Vmplus)
        
@@ -317,7 +317,6 @@ function gradient_conjugue(A,b,x0,kmax,e) result(x)
     allocate(Hm(m+1,m))
     allocate(Vmplus(n,m+1))
     allocate(Rm(m,m),Qm(m,m))
-  
     
     k=0
 
@@ -328,6 +327,8 @@ function gradient_conjugue(A,b,x0,kmax,e) result(x)
        
        Hm=0._pr
        Vmplus=0._pr
+       Qm=0._pr
+       Rm=0._pr
 
        !obtention de Hm et Vm+1
        call Arnoldi(r,A,Hm,Vmplus)
@@ -370,6 +371,219 @@ function gradient_conjugue(A,b,x0,kmax,e) result(x)
     deallocate(r,betae1,y,Hm,Vmplus,Rm,Qm,u)
     
   end function GMRes
+
+!==================================================================================================================================================
+  !fonction qui contient la methode de resolution FOM dans le cas particulier où la matrice du ssytéme linéaire est sdp
+
+  
+  function FOM_sdp(A,b,x0,kmax,e,m) result(x)
+
+    !variables d'entree
+    real(kind=pr),dimension(:,:),intent(in):: A       !matrice du systeme lineaire
+    real(kind=pr),dimension(:),intent(in):: b, x0     !vecteur second membre et donnée intiale
+    integer,intent(in):: kmax                         !test d'arret
+    real(kind=pr),intent(in):: e                      !precision
+    integer,intent(in):: m                            !caracterise espace de krylov dans lequel on se place
+
+    !variables de sortie
+    real(kind=pr),dimension(:),allocatable:: x        !solution approchée du systeme
+
+    !variables locales
+    real(kind=pr),dimension(:),allocatable:: r        !residu reel
+    real(kind=pr),dimension(:),allocatable:: y, u, betae1
+    real(kind=pr):: beta, somme
+    integer:: k, n, i, p
+    real(kind=pr),dimension(:,:),allocatable:: Hm, Vmplus    !matrice de Hessenberg et matrice de la nouvelle base orthogonal extrait de A à partir du résidu
+    real(kind=pr),dimension(:,:),allocatable:: L, tL    ! matrice de decomposition de cholesky de Hmbarre 
+
+    !initialisation
+    n=size(A(:,1))
+    allocate(r(n),x(n))
+    x=x0
+    r=b-MATMUL(A,x0)     !residu initial
+    beta=NORM2(r)
+    
+    allocate(y(m))
+    y=0._pr
+    
+    allocate(Hm(m+1,m))
+    allocate(Vmplus(n,m+1))
+
+    
+    allocate(L(m,m),tL(m,m))
+    
+    allocate(u(m),betae1(m))
+    u=0._pr
+    betae1=0._pr
+    betae1(1)=beta
+
+    k=0
+    
+    !algorithme de la methode
+    
+    do while (beta>e.and.k<= kmax)
+
+       Hm=0._pr
+       Vmplus=0._pr
+       L=0._pr
+       tL=0._pr
+       !obtention de Hm et Vm par la methode d'Arnoldi ameliore dans le cas ou A est sdp en partant de r et A
+       call Arnoldi_tridiag(r,A,Hm,Vmplus)
+
+       !obtention de la decomposition de Cholesky amelioree de Hm
+       L=cholesky_tridiag(Hm(1:m,1:m))
+       
+       !resolution de Hmbarre*y=beta*e1
+       !resolution de Lu=betae1
+       !L est triangulaiure inferieure
+       !methode de montee
+       u(1)=1._pr/L(1,1)*betae1(1)
+       do i=2,m
+          somme=0._pr
+          do p=1,i-1
+             somme=somme+L(i,p)*u(p)
+          end do
+          u(i)=-somme/L(i,i)
+       end do
+       
+       !resolution de tLy=u
+       !tL est triangulaire superieure a diagonale non nulle
+       !methode de descente
+       tL=transpose(L)
+       y(m)=1._pr/tL(m,m)*u(m)
+       do i=m-1,1,-1
+          somme=0._pr
+          do p=i+1,m
+             somme=somme+tL(i,p)*y(p)
+          end do
+          y(i)=(u(i)-somme)/tL(i,i)
+       end do
+        
+       x=x+MATMUL(Vmplus(1:n,1:m),y)   
+       r=-Hm(m+1,m)*y(m)*Vmplus(1:n,m+1)
+       beta=NORM2(r)
+       print*, beta, y(m), Hm(m+1,m)
+       k=k+1    
+    end do
+    
+    if (k>kmax) then
+       print*, 'tolerance non atteinte' , beta
+    else
+       print*, "convergence en :", k, "iterations"
+    end if
+
+    deallocate(r,y,Hm,Vmplus,L,tL,u,betae1)
+    
+  end function FOM_SDP
+!==================================================================================================================================================
+
+!fonction pour la methode GMRes dans le cas ou la matrice du ssyteme lineaire est sdp
+
+  
+  function GMRes_sdp(A,b,x0,kmax,e,m)  result(x)
+
+    !variables d'entree
+    real(kind=pr),dimension(:,:),intent(in):: A       !matrice de résolution du probleme
+    real(kind=pr),dimension(:),intent(in):: b, x0     !vecteur second membre et donnée intiale
+    integer,intent(in):: kmax                         !test d'arret
+    real(kind=pr),intent(in):: e                      !precision
+    integer,intent(in):: m
+
+    !variables de sortie
+    real(kind=pr),dimension(:),allocatable:: x        !solution approchee du systeme
+
+    !variables locales
+    real(kind=pr),dimension(:),allocatable:: r, y, betae1, u
+    real(kind=pr):: beta
+    integer:: n, k, p, i
+    real(kind=pr):: somme
+    real(kind=pr),dimension(:,:),allocatable:: Hm, Vmplus, L, tL     
+    
+    
+    !initialisation
+    n=size(A(:,1))
+    
+    allocate(x(n),r(n))
+    x=x0
+    r=b-MATMUL(A,x0)
+    beta=NORM2(r)
+    !definition de betae1
+    allocate(betae1(m),u(m))
+    u=0._pr
+    betae1=0._pr
+    betae1(1)=beta
+
+    
+    allocate(y(m))
+    y=0._pr
+    
+    allocate(Hm(m+1,m))
+    allocate(Vmplus(n,m+1))
+    allocate(L(m,m),tL(m,m))
+    
+    k=0
+
+    !algorithme de la  methode
+
+    do while (beta>e.and.k<= kmax)
+       
+       Hm=0._pr
+       Vmplus=0._pr
+       L=0._pr
+       tL=0._pr
+
+       !obtention de Hm et Vm+1 par la methode d'Arnoldi amelioree 
+       call Arnoldi_tridiag(r,A,Hm,Vmplus)
+
+       !obtention de la decomposition de Cholesky amelioree de Hm
+       L=cholesky_tridiag(Hm(1:m,1:m))
+       
+       !calcul de y=argmin(beta e1 - Hmy)
+       !resolution de Hmbarre*y=beta*e1
+
+       !resolution de Lu=betae1
+       !L est triangulaiure inferieure
+       !methode de montee
+       u(1)=1._pr/L(1,1)*betae1(1)
+       do i=2,m
+          somme=0._pr
+          do p=1,i-1
+             somme=somme+L(i,p)*u(p)
+          end do
+          u(i)=-somme/L(i,i)
+       end do
+       
+       !resolution de tLy=u
+       !tL est triangulaire superieure a diagonale non nulle
+       !methode de descente
+       tL=transpose(L)
+       y(m)=1._pr/tL(m,m)*u(m)
+       do i=m-1,1,-1
+          somme=0._pr
+          do p=i+1,m
+             somme=somme+tL(i,p)*y(p)
+          end do
+          y(i)=(u(i)-somme)/tL(i,i)
+       end do
+       
+       x=x+MATMUL(Vmplus(1:n,1:m),y)  
+       r=r-MATMUL(Vmplus,MATMUL(Hm,y))
+       beta=NORM2(betae1-MATMUL(Hm(1:m,1:m),y))
+       k=k+1
+       
+    end do
+
+    
+    if (k>kmax) then
+       print*, 'tolerance non atteinte', beta
+    else
+       print*, "convergence en :", k, "iterations"
+    end if
+
+    deallocate(r,betae1,y,Hm,Vmplus,u,L,tL)
+    
+  end function GMRes_Sdp
+
 !===================================================================================================================================================
 
   !algorithme d'Arnoldi: methode d'orthogonalisation de Gram-Schmidt modifiee
@@ -419,6 +633,60 @@ function gradient_conjugue(A,b,x0,kmax,e) result(x)
     deallocate(wj,vj)
     
   end subroutine Arnoldi
+
+!==========================================================================================================================================
+  !contient l'algorithme d'Arnoldi ameliore dans les cas ou la matrice obtenue est tridiagonale+derniere ligne
+   subroutine Arnoldi_tridiag(r,A,Hm,Vmplus)
+
+    !variables d'entrees
+    real(kind=pr),dimension(:,:),intent(in):: A
+    real(kind=pr),dimension(:),intent(in)::r                !vecteur qui definit l'espace de Krylov {v,Av,.....,A(m-1)v}
+   
+    !variables de sortie
+    real(kind=pr),dimension(:,:),intent(inout):: Vmplus    !matrice qui contient les vecteurs de la nouvelle base orthonormale
+    real(kind=pr),dimension(:,:),intent(inout):: Hm     !matrice de Hessenberg qui contient les coefficients de la methode de Gram-Schmidt
+    
+    !variables locales
+    integer:: n, m, i, j
+    real(kind=pr),dimension(:),allocatable:: wj, vj
+   
+    !initialisation
+    n=size(A(:,1))
+    m=size(Hm(1,:))
+    
+    allocate(wj(n),vj(n))
+    Hm=0._pr
+    Vmplus=0._pr
+
+    !algorithme d'Arnoldi
+
+    Vmplus(1:n,1)=r/NORM2(r)
+    do j=1,m
+       vj=Vmplus(:,j)
+       wj=MATMUL(A,vj)
+       if (j==1) then
+          Hm(j,j)=DOT_PRODUCT(wj,Vmplus(:,j))
+          wj=wj-Hm(j,j)*Vmplus(:,j)
+       else
+          do i=j-1,j
+             Hm(i,j)=DOT_PRODUCT(wj,Vmplus(:,i))
+             wj=wj-Hm(i,j)*Vmplus(:,i)
+          end do
+       end if
+      
+       Hm(j+1,j)=NORM2(wj)
+
+       if( Hm(j+1,j)==0) then
+          stop
+       end if
+       Vmplus(:,j+1)=wj/Hm(j+1,j)
+       
+    end do
+    deallocate(wj,vj)
+    
+  end subroutine Arnoldi_Tridiag
+
+  
 
 !===========================================================================================================================================
 
@@ -488,7 +756,47 @@ function gradient_conjugue(A,b,x0,kmax,e) result(x)
 
   end subroutine QR
 
-  !===========================================================================================================================================
+!===========================================================================================================================================
+  !contient la decomposition de Cholesky d'une matrice tridiagonale sdp
+
+  function cholesky_tridiag(A) result(L)
+
+    !variables d'entree
+    real(kind=pr),dimension(:,:),intent(in):: A
+    !variables de sortie
+    real(kind=pr),dimension(:,:),allocatable:: L
+    !variables locales
+    integer:: m, j, k
+    real(kind=pr):: somme1, somme2
+
+    !initialisation
+    m=size(A(1,:))
+    allocate(L(m,m))
+    L=0._pr
+   
+    !algorithme de decomposition remplissage de L
+    !pour j=1
+    L(1,1)=sqrt(A(1,1))
+    L(2,1)=A(2,1)/L(1,1)
+    !cas general
+    do j=2,m-1
+        somme1=0._pr
+        somme2=0._pr
+       do k=1,j-1
+          somme1=somme1+L(j,k)**2
+          somme2=somme2+L(j+1,j)*L(j,k)
+       end do
+       L(j,j)=sqrt(A(j,j)-somme1)
+       L(j+1,j)=(A(j+1,j)-somme2)/L(j,j)
+    end do
+    !cas j=m
+    do k=1,m-1
+       somme1=somme1+L(m,k)**2
+    end do
+    L(m,m)=sqrt(A(m,m)-somme1)/L(m,m)
+
+  end function cholesky_tridiag
+!===========================================================================================================================================
   !Fonction pour le calcul de la normeInf
 
   ! function norme_inf (a) result(norme)
