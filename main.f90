e1cf2f7eaf5eff1713e4cbbab14212d81b65f3d4
PraudMaelle <75522401+PraudMaelle@users.noreply.github.com>
2020-12-16 18:18:32 +0100

    Update main.f90

--- main.f90
+++ main.f90
@@ -17,7 +17,7 @@ program main
   real(kind=pr),dimension(:,:),allocatable:: Bn, An, Tn
   real(kind=pr),dimension(:),allocatable:: tab
   real(kind=pr):: alpha, sommetBnBn
-  integer:: i, j
+  integer:: i, j, k
 !=================================================================================================================================================================
 
 
@@ -25,56 +25,6 @@ program main
   print*, "ou A est une matrice symetrique definie positive de taille n"
   print*, "les methodes calculent une solution approchee du systeme avec une certaine precision "
 
-  
-!=====================================================================================================================================================================
-  !premiers tests sur une matrice petite et simple A symetrique et definie positive 
-  !initialisation
-  e=0.0001
-  kmax=100000
-  m=4
-  n=10
-  allocate(A(n,n),b(n),x0(n),x(n))
-
-  !remplissage de la matrice
-  A=1._pr
-  do i=1,n
-     A(i,i)=2._pr
-  end do
-
-  !vecteur second membre
-  do i=1,n
-     b(i)=i
-  end do
-
-  !vecteur initial de depart
-  x0=0._pr
-  
-  !affichage des resultats
-  print*, "'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''"
-  print*, "test des methodes de resolution Ax=b  pour une matrice simple"
-  print*, "taille du systeme:", n
-  print*,"precision:", e
-  print*, "m:", m
-  print*, "methode du gradient a pas optimal"
-  x=grad_pas_optimal(A,b,x0,kmax,e)
-  print*, x
-  print*, "methode du residu minimum"
-  x=res_min(A,b,x0,kmax,e)
-  print*, x
-  print*, "methode du gradient conjugue"
-  x=gradient_conjugue(A,b,x0,kmax,e)
-  print*, x
-  print*, "methode FOM"
-  !x=FOM(A,b,x0,kmax,e,m)
- ! print*, x
-  print*, "methode GMRes"
-  x=GMRes(A,b,x0,kmax,e,m)
-  print*, x
-  print*, "methode GMRes amelioree pour une matrice sdp"
-  x=GMRes_sdp(A,b,x0,kmax,e,m)
-  print*,x
-  
-  deallocate(A,b,x0,x)
 !====================================================================================================================================================
   !tests de FOM et GMRes classiques sur des matrices non sdp de petite taille
   print*, "'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''"
@@ -90,11 +40,17 @@ program main
   print*, "tests sur une matrice de la forme An=In-alpha*tBn* Bn "
   print*, "saisir n la taille du systeme:"
   read*, n
-  print*, "saisir m pour definir l'espace de Krylov :"
-  read*, m
   print*, "saisir la precision voulue sur la solution:"
   read*, e
-
+  print*, "saisir la methode choisie:"
+  print*, "1)Gradient a pas optimal:"
+  print*, "2)Residu minimum:"
+  print*, "3)gradient conjugue:"
+  print*, "4)FOM:"
+  print*, "5)GMRes:"
+  read*, k
+  
+  
   kmax=1000000
   allocate(An(n,n),Bn(n,n),Tn(n,n),x0(n),x(n),b(n),tab(n))
 
@@ -121,6 +77,7 @@ program main
   end do
 
   alpha=MAXVAL(tab)
+ ! alpha=n*n
     
   !remplissage de An à partir de Bn
   do i=1,n
@@ -135,27 +92,31 @@ program main
   !vecteur initial de depart
   x0=0._pr
 
-  !affichage des resultats
-  print*, "'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''"
-  print*, "methode du gradient a pas optimal"
-  x=grad_pas_optimal(An,b,x0,kmax,e)
-  print*, "methode du residu minimum"
-  x=res_min(An,b,x0,kmax,e)
-  print*, "methode du gradient conjugue"
-  x=gradient_conjugue(An,b,x0,kmax,e)
- ! print*, x 
-  print*, "methode FOM"
- ! x=FOM(An,b,x0,kmax,e,m)
-  ! print*, x
-  print*, "methode FOM amelioree pour une matrice sdp"
-  !x=FOM_sdp(An,b,x0,kmax,e,m)
-  !print*, x
-  print*, "methode GMRes"
-  x=GMRes(An,b,x0,kmax,e,m)
-  print*, x
-  print*, "methode GMRes amelioree pour une matrice sdp"
-  x=GMRes_sdp(An,b,x0,kmax,e,m)
-  print*,x
+  select case (k)
+  case(1)
+     print*, "methode du gradient a pas optimal"
+     x=grad_pas_optimal(An,b,x0,kmax,e)
+  case(2)
+     print*, "methode du residu minimum"
+     x=res_min(An,b,x0,kmax,e)
+  case(3)
+     print*, "methode du gradient conjugue"
+     x=gradient_conjugue(An,b,x0,kmax,e)
+  case(4)
+     print*, "saisir m pour definir l'espace de Krylov :" 
+     read*, m
+     print*, "methode FOM"
+     x=FOM(An,b,x0,kmax,e,m)
+     print*, "methode FOM amelioree pour une matrice sdp"
+     x=FOM_sdp(An,b,x0,kmax,e,m)
+  case(5)
+     print*, "saisir m pour definir l'espace de Krylov :" 
+     read*, m
+     print*, "methode GMRes"
+     x=GMRes(An,b,x0,kmax,e,m)
+     print*, "methode GMRes amelioree pour une matrice sdp"
+     x=GMRes_sdp(An,b,x0,kmax,e,m)
+  end select
  
   deallocate(An,Bn,Tn,x0,x,b)
   
